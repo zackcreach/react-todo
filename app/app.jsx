33c354d00b935c78d79746a204b79611d5245eec
zackcreach <zack@peachdish.com>
2017-06-27 18:40:02 -0400

    Refactor app to work with localStorage.

--- app/app.jsx
+++ app/app.jsx
@@ -7,14 +7,18 @@ var TodoApp = require('TodoApp');
 
 var actions = require('actions');
 var store = require('configureStore').configure();
+var TodoAPI = require('TodoAPI');
 
 store.subscribe(() => {
+  var state = store.getState();
+
   console.log('New state', store.getState());
+
+  TodoAPI.setTodos(state.todos);
 });
 
-store.dispatch(actions.addTodo('Clean the yard'));
-store.dispatch(actions.setSearchText('yard'));
-store.dispatch(actions.toggleShowCompleted());
+var initialTodos = TodoAPI.getTodos();
+store.dispatch(actions.addTodos(initialTodos));
 
 // Load foundation
 $(document).foundation();
--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -47,7 +47,12 @@ export var todosReducer = (state = [], action) => {
         } else {
           return todo;
         }
-      })
+      });
+    case 'ADD_TODOS':
+      return [
+        ...state,
+        ...action.todos
+      ];
     default:
       return state;
   }
