93a766724451d6e9f4dcf32b8a358bf3e8028f94
zackcreach <zack@peachdish.com>
2017-06-28 17:16:05 -0400

    Move firebase credentials to env variables.

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyC1GBY5Bc6dz-2fIVGcfJ8oAIcQaTxhefc",
-    authDomain: "creach-todo-app-fc51b.firebaseapp.com",
-    databaseURL: "https://creach-todo-app-fc51b.firebaseio.com",
-    projectId: "creach-todo-app-fc51b",
-    storageBucket: "creach-todo-app-fc51b.appspot.com",
-    messagingSenderId: "832634864266"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
